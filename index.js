c2d946c6e9dec83d6e37b28c5738320aded5cb6e
Fhomi Zuenda <Zuenda25@gmail.com>
2021-01-08 16:52:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser')
 const db = require('./queries')
 const cors = require("cors");
 const app = express()
-const port = 3000
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 const path = __dirname + '/app/views/';
 
