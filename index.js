f0f651666e3b28499c381a96b57aa6aaa096a6a2
Fhomi Zuenda <Zuenda25@gmail.com>
2021-01-08 17:00:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const db = require('./queries')
 const cors = require("cors");
 const app = express()
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
 });
-
 const path = __dirname + '/app/views/';
 
 app.use(express.static(path));
